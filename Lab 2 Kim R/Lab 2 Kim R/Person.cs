ae42abec8176ae0524f170889a97175bfc32610e
xrankin05x <31674747+xrankin05x@users.noreply.github.com>
2017-10-09 19:19:12 -0400

    Lab 2

---
+++ Lab 2 Kim R/Lab 2 Kim R/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2_Kim_R
+{
+    class Person
+    {
+        public string FirstName;
+        public string LastName;
+        public int Age;
+        public string MaritalStatus;
+        public string Spouse;
+        public int AgeSpouse;
+        public string GetFullName()
+        {
+            return this.FirstName + "" +this.LastName;
+          
+        }
+
+        public int GetSumOfAllAges()
+        {
+            return this.Age ;
+        }
+
+
+
+        
+
+
+
+
+    }
+}
---
+++ Lab 2 Kim R/Lab 2 Kim R/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2_Kim_R
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            person1.FirstName = "Santa";
+            person1.LastName = "Claus";
+            person1.Age = 75;
+            person1.MaritalStatus = "yes";
+            person1.Spouse = "Sarah";
+            person1.AgeSpouse = 70;
+
+            Person person2 = new Person();
+            person2.FirstName = "Rudolph";
+            person2.LastName = "Deer";
+            person2.Age = 35;
+            person2.MaritalStatus = "yes";
+            person2.Spouse = "Ruby";
+            person2.AgeSpouse = 33;
+
+
+            
+
+            System.Console.WriteLine(person1.GetFullName());
+            System.Console.WriteLine(person2.GetFullName());
+
+            System.Console.WriteLine(person1.GetSumOfAllAges());
+            System.Console.WriteLine(person2.GetSumOfAllAges());
+
+            System.Console.WriteLine("Press any key. ..");
+            System.Console.ReadKey();
+
+
+            
+
+        }
+    }
+}
