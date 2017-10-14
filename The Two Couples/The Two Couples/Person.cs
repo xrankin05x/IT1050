f51d17b151ac710d712b3c1218aeff7e2e40dcff
xrankin05x <31674747+xrankin05x@users.noreply.github.com>
2017-10-14 18:34:31 -0400

    The Two

---
+++ The Two Couples/The Two Couples/Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Two_Couples
+{
+    class Person
+    {
+        public string FirstName;
+        public string LastName;
+        public int Age;
+        public string MartialStatus;
+        public string Spouse;
+        public int YearsOld;
+
+        public static double SumOfAllAges;
+
+
+        public string GetFullName()
+        {
+            string fullName = "";
+            fullName += this.FirstName;
+            fullName += "  ";
+            fullName += " ";
+            fullName += this.LastName;
+            return fullName;
+        }
+            
+                public int AverageAllAges()
+        {
+            return this.Age + YearsOld / 4;
+        }
+           public void PrintNameAndAge()
+        {
+                       
+            System.Console.WriteLine("My name is " + this.GetFullName());
+            System.Console.WriteLine("My age is " + this.Age);
+
+        }
+        
+
+
+    }
+}
---
+++ The Two Couples/The Two Couples/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Two_Couples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            person1.FirstName = "Santa";
+            person1.LastName = "Claus";
+            person1.Age = 80;
+            person1.MartialStatus = "yes";
+            person1.Spouse = "Sara";
+            person1.YearsOld = 76;
+
+            Person person2 = new Person();
+            person2.FirstName = "Rudolph";
+            person2.LastName = "Reindeer";
+            person2.Age = 48;
+            person2.MartialStatus = "yes";
+            person2.Spouse = "Rose";
+            person2.YearsOld = 40;
+
+
+
+            person1.PrintNameAndAge();
+            person2.PrintNameAndAge();
+
+           
+            System.Console.WriteLine(person1.AverageAllAges());
+            System.Console.WriteLine(person2.AverageAllAges());
+            
+
+            System.Console.WriteLine("Press any key to continue...");
+            System.Console.ReadKey();
+        }
+    }
+}
