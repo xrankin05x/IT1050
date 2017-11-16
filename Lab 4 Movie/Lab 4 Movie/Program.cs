1acd4a803b8577deb29293e2f49856e392c923ad
xrankin05x <31674747+xrankin05x@users.noreply.github.com>
2017-11-16 18:38:14 -0500

    lab 4 movies

---
+++ Lab 4 Movie/Lab 4 Movie/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4_Movie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+             double ticketMatineeChild = 3.99;
+             double ticketMatineeAdult = 5.99;
+             double ticketMatineeSenior = 4.50;
+             double ticketEveningChild = 6.99;
+             double ticketEveningAdult = 10.99;
+             double ticketEveningSenior = 8.50;
+
+             double foodSmallSoda = 3.50;
+             double foodLargeSoda = 5.99;
+             double foodHotDog = 3.99;
+             double foodPopcorn = 4.50;
+             double foodCandy = 1.99;
+
+            double addticketMatineeChild;
+            double addticketMatineeAdult;
+            double addticketMatineeSenior;
+            double add
+
+
+
+            System.Console.Write("How many child matinee tickets? ");
+            ticketMatineeChild = double.Parse(System.Console.ReadLine());
+
+            System.Console.Write("How many adult matinee tickets? ");
+            ticketMatineeAdult = double.Parse(System.Console.ReadLine());
+
+            System.Console.Write("How many senior matinee tickets? ");
+            ticketMatineeSenior = double.Parse(System.Console.ReadLine());
+
+            System.Console.Write("How many child evening tickets? ");
+            ticketEveningChild = double.Parse(System.Console.ReadLine());
+
+            System.Console.Write("How many adult evening tickets? ");
+            ticketEveningAdult = double.Parse(System.Console.ReadLine());
+
+            System.Console.Write("How many senior evening tickets? ");
+            ticketEveningSenior = double.Parse(System.Console.ReadLine());
+
+            System.Console.Write("How many small sodas? ");
+            foodSmallSoda = double.Parse(System.Console.ReadLine());
+
+            System.Console.Write("How many large sodas? ");
+            foodLargeSoda = double.Parse(System.Console.ReadLine());
+
+            System.Console.Write("How many Popcorn? ");
+            foodPopcorn = double.Parse(System.Console.ReadLine());
+
+            System.Console.Write("How many hot dogs? ");
+            foodHotDog = double.Parse(System.Console.ReadLine());
+
+            System.Console.Write("How many candy? ");
+            foodCandy = double.Parse(System.Console.ReadLine());
+
+            double totalMoviePurchase = 0;
+
+
+
+            totalMoviePurchase += addticketMatineeChild 
+            totalMealCalories += addCarnitas * caloriesCarnitas;
+
+            totalMealCalories += addSteak * caloriesSteak;
+
+            totalMealCalories += addBarbacoa * caloriesBarbacoa;
+
+            totalMealCalories += addTortilla * caloriesTortilla;
+
+            totalMealCalories += addTacoShells * caloriesTacoShells;
+
+            totalMealCalories += addRice * caloriesRice;
+
+            totalMealCalories += addBeans * caloriesBeans;
+
+            totalMealCalories += addFajita * caloriesFajita;
+
+            totalMealCalories += addGuacamole * caloriesGuacamole;
+
+            totalMealCalories += addSourCream * caloriesSourCream;
+
+            totalMealCalories += addCheese * caloriesCheese;
+
+            totalMealCalories += addCornSalsa * caloriesCornSalsa;
+
+            totalMealCalories += addOtherSalsa * caloriesOtherSalsa;
+
+
+
+
+            if (concessionPopcorn + concessionLargeSoda - 2.00 )
+            {
+
+            }
+            System.Console.WriteLine("Press any key to continue...");
+            System.Console.ReadKey();
+        }
+    }
+}
